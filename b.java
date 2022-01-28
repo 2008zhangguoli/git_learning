5bbd5563420156a5216e443405d66df04c3db7e7
zhangguoli <zhangguoli@m800.com>
2022-01-28 14:55:14 +0800

    Add VSCode change

--- b.java
+++ b.java
@@ -3,3 +3,8 @@
 // zheshi zai Terminal zhong xiugai de 
 // 2022年01月28日11:53:51 在Terminal中修改
 // 2022年01月28日11:54:26 在 VSCode上修改
+
+// 2022年01月28日14:53:43 第二次在VSCode上修改开始
+// 内容1 VSCode
+// 内容2 VSCode
+// 2022年01月28日14:53:43 第二次在VSCode上修改 结束
